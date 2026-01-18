21ddc9674d94385c73b1bc80206e723870461cf1
Umar Akram <uamirad00@gmail.com>
2026-01-18 20:21:57 +0200

    Adding Constrains for SubViews in View Controller, and added fading of UIViews except for one tapped with animation.

--- Traffic Light Color Picker/TrafficView.swift
+++ Traffic Light Color Picker/TrafficView.swift
@@ -9,4 +9,26 @@ import UIKit
 
 class TrafficView: UIView {
     
+    var color: UIColor!
+    
+    init(color: UIColor) {
+        super.init(frame: .zero)
+        self.color = color
+        setup()
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        layer.cornerRadius = 200 / 2
+        clipsToBounds = true
+        backgroundColor = color
+        alpha = 0.25
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
 }
--- Traffic Light Color Picker/ViewController.swift
+++ Traffic Light Color Picker/ViewController.swift
@@ -8,12 +8,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var redLight = TrafficView(color: .systemRed)
+    var yellowLight = TrafficView(color: .systemYellow)
+    var greenLight = TrafficView(color: .systemGreen)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        configure()
+
+        addTapGestureRecognizer(to: redLight)
+        addTapGestureRecognizer(to: yellowLight)
+        addTapGestureRecognizer(to: greenLight)
     }
-
-
+    
+    
+    func addTapGestureRecognizer(to view:UIView){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func handleTap(_ gesture: UITapGestureRecognizer){
+        
+        
+        if let tappedView = gesture.view {
+            UIView.animate(withDuration: 0.25) {
+                self.view.subviews.forEach { view in
+                    view.alpha = 0.25
+                }
+                tappedView.alpha = 1
+            }
+           
+        }
+    }
+    func configure(){
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(redLight)
+        view.addSubview(yellowLight)
+        view.addSubview(greenLight)
+        
+        let padding:CGFloat = 40
+        let size:CGFloat = 200
+        
+        
+        NSLayoutConstraint.activate([
+            redLight.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+            redLight.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            redLight.widthAnchor.constraint(equalToConstant: size),
+            redLight.heightAnchor.constraint(equalToConstant: size),
+            
+            yellowLight.topAnchor.constraint(equalTo: redLight.bottomAnchor, constant: padding),
+            yellowLight.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            yellowLight.widthAnchor.constraint(equalToConstant: size),
+            yellowLight.heightAnchor.constraint(equalToConstant: size),
+            
+            greenLight.topAnchor.constraint(equalTo: yellowLight.bottomAnchor, constant: padding),
+            greenLight.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            greenLight.widthAnchor.constraint(equalToConstant: size),
+            greenLight.heightAnchor.constraint(equalToConstant: size),
+            
+            
+        ])
+    }
+    
+    
+    
+    
+    
+    
 }
 
